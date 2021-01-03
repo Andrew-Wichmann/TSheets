96cc6f3b8ea209c87a30573dd010ec102903ef96
Andrew <andrewp.wichmann@gmail.com>
2021-01-02 19:03:05 -0600

    add sentry

--- TSheetsAutomation/TSheetsAutomation/settings.py
+++ TSheetsAutomation/TSheetsAutomation/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -177,3 +180,12 @@ LOGGING = {
         },
     },
 }
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
